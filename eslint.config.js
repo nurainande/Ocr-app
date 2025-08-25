f4eddef25a0e341b53a3a7ffd8ec4b550382dd53
nurainande <nurainande@gmail.com>
2025-08-25 14:47:21 +0100

    Second commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import js from '@eslint/js'
+
 import globals from 'globals'
+
 import reactHooks from 'eslint-plugin-react-hooks'
+
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
